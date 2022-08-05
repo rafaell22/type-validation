f15dae4c1501a47dbececb51f1306368a0836ec3
rafaellfpc <rafaell@fleetprofitcenter.com>
2022-08-05 09:26:28 -0500

    Add validation for array items

--- validation.js
+++ validation.js
@@ -8,6 +8,23 @@ function toRawType (value) {
 }
 
 /**
+ * Structure for a validation error
+ * @param       {string} error  Error message
+ * @param       {string} type   Type of the validation that failed
+ * @param       {any}    value  Value that didn't pass the validation
+ * @constructor
+ */
+class ValidationError extends Error {
+  constructor(message, type, value, error) {
+    super(message);
+    this.message = message;
+    this.error = error;
+    this.type = type;
+    this.value = value;
+  }
+}
+
+/**
  * Schema for variable type validation. Throws error if the validation fails
  * @param {any} value   Value to be validated
  * @constructor
@@ -22,7 +39,11 @@ function Schema(value) {
  */
 Schema.prototype.defined = function() {
     if(typeof this.value === typeof void 0) {
-        throw new Error('Value is UNdefined.');
+        throw new ValidationError(
+            'Value is UNdefined.',
+            'undefined',
+            this.value
+        );
     }
     return this;
 }
@@ -33,7 +54,11 @@ Schema.prototype.defined = function() {
  */
 Schema.prototype.undefined = function() {
     if(typeof this.value !== typeof void 0) {
-        throw new Error('Value is Defined.');
+        throw new ValidationError(
+            'Value is Defined.',
+            'defined',
+            this.value
+        );
     }
     return this;
 }
@@ -44,7 +69,11 @@ Schema.prototype.undefined = function() {
  */
 Schema.prototype.null = function() {
     if(this.value !== null) {
-        throw new Error('Value is NOT Null.');
+        throw new ValidationError(
+            'Value is NOT Null.',
+            'null',
+            this.value
+        );
     }
     return this;
 }
@@ -55,7 +84,11 @@ Schema.prototype.null = function() {
  */
 Schema.prototype.notNull = function() {
     if(this.value === null) {
-        throw new Error('Value is Null.');
+        throw new ValidationError(
+            'Value is Null.',
+            'notNull',
+            this.value
+        );
     }
     return this;
 }
@@ -69,7 +102,11 @@ Schema.prototype.function = function() {
         toRawType(this.value) !== 'Function' &&
         toRawType(this.value) !== 'AsyncFunction'
     ) {
-        throw new Error('Value is NOT a Function.');
+        throw new ValidationError(
+            'Value is NOT a Function.',
+            'function',
+            this.value
+        );
     }
     return this;
 }
@@ -92,7 +129,11 @@ NumberValidation.prototype.min = function(limit) {
     try {
         validate(limit).number();
         if(this.value < limit) {
-            throw new Error(`Value is less than ${limit}`);
+            throw new ValidationError(
+                `Value is less than ${limit}`,
+                'number.min',
+                this.value
+            );
         }
         
         return this;
@@ -110,7 +151,11 @@ NumberValidation.prototype.max = function(limit) {
     try {
         validate(limit).number();
         if(this.value > limit) {
-            throw new Error(`Value is more than ${limit}`);
+            throw new ValidationError(
+                `Value is more than ${limit}`,
+                'number.max',
+                this.value
+            );
         }
         
         return this;
@@ -125,7 +170,11 @@ NumberValidation.prototype.max = function(limit) {
  */
 NumberValidation.prototype.positive = function() {
     if(this.value <= 0) {
-        throw new Error('Value is NOT Positive');
+        throw new ValidationError(
+            'Value is NOT Positive',
+            'number.positive',
+            this.value
+        );
     }
     
     return this;
@@ -141,7 +190,11 @@ Schema.prototype.number = function() {
             toRawType(this.value) !== 'Number' ||
             isNaN(this.value)
         ) {
-            throw new Error('Value is NOT a Number');
+            throw new ValidationError(
+                'Value is NOT a Number',
+                'number',
+                this.value
+            );
         }
         return new NumberValidation(this.value);
     } catch(errorValidatingNumber) {
@@ -164,7 +217,11 @@ function StringValidation(value) {
  */
 StringValidation.prototype.notEmpty = function() {
     if(this.value === '') {
-        throw new Error('Value is an empty String');
+        throw new ValidationError(
+            'Value is an empty String',
+            'string.notEmpty',
+            this.value
+        );
     }
     
     return this;
@@ -179,7 +236,11 @@ StringValidation.prototype.maxLength = function(limit) {
     try {
         validate(limit).number();
         if(this.value.length > limit) {
-            throw new Error(`Value's length is more than ${limit}`);
+            throw new ValidationError(
+                `Value's length is more than ${limit}`,
+                'string.maxLength',
+                this.value
+            );
         }
         
         return this;
@@ -197,7 +258,11 @@ Schema.prototype.string = function() {
         if(
             toRawType(this.value) !== 'String'
         ) {
-            throw new Error('Value is NOT a String');
+            throw new ValidationError(
+                'Value is NOT a String',
+                'string',
+                this.value
+            );
         }
         return new StringValidation(this.value);
     } catch(errorValidatingString) {
@@ -214,7 +279,11 @@ Schema.prototype.boolean = function() {
         this.value !== false &&
         this.value !== true
     ) {
-        throw new Error('Value is NOT a Boolean.');
+        throw new ValidationError(
+            'Value is NOT a Boolean.',
+            'boolean',
+            this.value
+        );
     }
     return this;
 }
@@ -227,8 +296,79 @@ Schema.prototype.object = function() {
     if(
       toRawType(this.value) !== 'Object'
     ) {
-        throw new Error('Value is NOT an Object.');
+        throw new ValidationError(
+            'Value is NOT an Object.',
+            'object',
+            this.value
+        );
     }
+    return this;
+}
+
+/**
+ * Schema to validate an Array's value.
+ * @param {Array} value  Value to be validated
+ * @constructor
+ */
+function ArrayValidation(value) {
+    this.value = value;
+}
+
+/**
+ * Validate the type of the items in an array
+ * @param  {Function/string} callback               Callback function or method names to validate the array items against. For multiple methods, use dot notation ('number.integer')
+ * @return {ArrayValidation}                        Instance of the array validation schema
+ */
+ArrayValidation.prototype.items = function(callback) {
+    let itemsValidation;
+    try {
+      validate(callback).function();
+      itemsValidation = (function() {
+        for (const item of this.value) {
+            try {
+                callback(item);
+            } catch(errorValidatingItem) {
+                throw errorValidatingItem;
+            }
+        }
+      }).bind(this);
+    } catch (errorValidatingFunction) {
+      try {
+        validate(callback).string();
+        itemsValidation = (function() {
+          const methods = callback.split('.');
+          
+          for (const item of this.value) {
+              try {
+                  let validation = validate(item);
+                  for(let methodIndex = 0; methodIndex < methods.length; methodIndex++) {
+                    validation = validation[methods[methodIndex]]()
+                  }
+              } catch(errorValidatingItem) {
+                  throw errorValidatingItem;
+              }
+          }
+        }).bind(this);
+      } catch (errorValidatingString) {
+        throw new ValidationError(
+            'Callback argument is NOT valid',
+            'items',
+            this.value,
+            errorValidatingString
+        );
+      }
+    }
+    
+    try {
+      itemsValidation();
+    } catch (errorValidatingItems) {
+      throw new ValidationError(
+          errorValidatingItems,
+          'items',
+          this.value,
+      );
+    }
+    
     return this;
 }
 
@@ -240,9 +380,13 @@ Schema.prototype.array = function() {
     if(
       toRawType(this.value) !== 'Array'
     ) {
-        throw new Error('Value is NOT an Array.');
+        throw new ValidationError(
+            'Value is NOT an Array.',
+            'array',
+            this.value
+        );
     }
-    return this;
+    return new ArrayValidation(this.value);
 }
 
 /**
@@ -253,7 +397,11 @@ Schema.prototype.htmlElement = function() {
     if(
       !this.value instanceof HTMLElement
     ) {
-        throw new Error('Value is NOT an HTMLElement.');
+        throw new ValidationError(
+            'Value is NOT an HTMLElement.',
+            'htmlElement',
+            this.value
+        );
     }
     return this;
 }
@@ -266,7 +414,11 @@ StringValidation.prototype.values = NumberValidation.prototype.values = Schema.p
     if(
       !args.some(value => value === this.value)
     ) {
-        throw new Error('Value is NOT in list of allowed values.');
+        throw new ValidationError(
+            'Value is NOT in list of allowed values.',
+            'values',
+            this.value
+        );
     }
     return this;
 }
